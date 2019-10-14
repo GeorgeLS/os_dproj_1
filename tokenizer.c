a08d0020a9052a51984b725a3c46bd515650bb32
george <sdi1600094@di.uoa.gr>
2019-10-14 22:35:08 +0300

    minor stuff

--- tokenizer.c
+++ tokenizer.c
@@ -5,16 +5,23 @@ bool tokenizer_has_next(tokenizer *tokenizer) {
 }
 
 token tokenizer_next_token(tokenizer *tokenizer) {
+  size_t length = tokenizer->length;
+  char delimiter = tokenizer->delimiter;
   size_t old_pos = tokenizer->index;
   size_t current_index = tokenizer->index;
-  while (current_index != tokenizer->length &&
-      tokenizer->stream[current_index] != tokenizer->delimiter) {
+  while (current_index != length &&
+      tokenizer->stream[current_index] != delimiter) {
+    ++current_index;
+  }
+
+  while (current_index != length &&
+      tokenizer->stream[current_index] == delimiter) {
     ++current_index;
   }
 
   tokenizer->index = current_index == tokenizer->length
-      ? current_index
-      : current_index + 1U;
+                     ? current_index
+                     : current_index + 1U;
 
   return (token) {
       .string = &tokenizer->stream[old_pos],
@@ -23,11 +30,13 @@ token tokenizer_next_token(tokenizer *tokenizer) {
 }
 
 size_t tokenizer_remaining_tokens(tokenizer *tokenizer) {
-  if (tokenizer->index == tokenizer->length) return 0U;
+  size_t length = tokenizer->length;
+  if (tokenizer->index == length) return 0U;
   size_t current_index = tokenizer->index;
   size_t remaining_tokens = 1U;
-  while (current_index != tokenizer->length) {
-    if (tokenizer->stream[current_index] == tokenizer->delimiter) {
+  char delimiter = tokenizer->delimiter;
+  while (current_index != length) {
+    if (tokenizer->stream[current_index] == delimiter) {
       ++remaining_tokens;
     }
     ++current_index;
